0de9db158f30405f56ddd977c77f48f10b9b7714
ManSino96 <mazisino@icloud.com>
2020-09-10 20:40:43 +0200

    Address Updated

--- Patient.java
+++ Patient.java
@@ -6,16 +6,19 @@ public class Patient{
     String surname ;
     String ID_number;
     char gender;  // F or M.
+    String address;
 
-    public  Patient(String name_, String surname_, String ID_number_, int age_, char gender_){
+    public  Patient(String name_, String surname_, String ID_number_, int age_, char gender_, String address_){
         name = name_;
         surname = surname_;
         ID_number = ID_number_;
         age = age_;
         gender = gender_;
+        address = address_; 
     }
 
   public void displayDetails() {
       System.out.printf(name  + " " + surname + " " + ID_number + " " + gender + " of age: " + age + "\n");
+      System.out.printf(address + "\n\n");
  }
 }
--- main.java
+++ main.java
@@ -1,11 +1,9 @@
-import java.io.PrintStream; 
-
 public class main{
 
 
     public static void main(String[] args) {
-        Patient patient1 = new Patient("Sino", "Mazi", "ZZ6690", 23, 'M');
-        Patient patient2  = new Patient("Mino", " Nazi", "ZZ5589", 24, 'F');
+        Patient patient1 = new Patient("Sino", "Mazi", "ZZ6690", 23, 'M', "5 Munga Street");
+        Patient patient2  = new Patient("Mino", " Nazi", "ZZ5589", 24, 'F', "80 Shidavura Street");
 
           patient1.displayDetails();
           patient2.displayDetails();
